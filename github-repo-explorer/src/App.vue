cef059d8745bada4dab042a6317e9db670a5981a
zach <z.ach@outlook.com>
2016-01-13 20:21:24 +0800

    add github-repo-explorer

---
+++ github-repo-explorer/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <form id="changeRepoForm" @submit.prevent="changeRepo()" class="form-inline">
+            <div class="form-group">
+              <label for="fullRepoName">Full Repo Name</label>
+              <input type="text" name="fullRepoName" v-model="fullRepoName" class="form-control">
+            </div>
+            <input type="submit" class="btn btn-default" value="Get repo filesystem!">
+          </form>
+          <explorer :username="username" :repo="repo"></explorer>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Explorer from './Explorer.vue'
+
+export default {
+  data () {
+    return {
+      fullRepoName: '',
+      username: '',
+      repo: ''
+    }
+  },
+  methods: {
+    changeRepo() {
+      [ this.username, this.repo ] = this.fullRepoName.split('/');
+    }
+  },
+  components: {
+    Explorer
+  }
+}
+</script>
+
+<style>
+body {
+  padding-top: 30px;
+  font-family: Helvetica, sans-serif;
+}
+#changeRepoForm {
+  margin-bottom: 30px;
+}
+</style>
---
+++ github-repo-explorer/src/Explorer.vue
@@ -0,0 +1,104 @@
+<template>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table">
+            <caption>{{ path }}</caption>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th class="text-right"><button class="btn btn-default btn-xs" @click="goBack()" v-if="path !== '/'">Go Back</button></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="file in sortedFiles">
+                    <td>
+                        <div class="file" v-if="file.type === 'file'">
+                            <i class="fa fa-file-o"></i>
+                            {{ file.name }}
+                        </div>
+                        <div class="directory" v-if="file.type === 'dir'">
+                            <i class="fa fa-folder-o"></i>
+                            <a @click="changePath(file.path)">{{ file.name }}</a>
+                        </div>
+                    </td>
+                    <td class="text-right">
+                        <a href="{{ file.download_url }}" download v-if="file.type === 'file'">
+                            <i class="fa fa-cloud-download"></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      path: '/',
+      files: []
+    }
+  },
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    repo: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    sortedFiles() {
+      return this.files.slice(0).sort((a,b) => {
+        if (a.type !== b.type) {
+          if (a.type === 'dir') {
+            return -1;
+          } else {
+            return 1;
+          }
+        } else {
+          if (a.name < b.name) {
+            return -1;
+          } else {
+            return 1;
+          }
+        }
+      });
+    }
+  },
+  methods: {
+    fetchFiles() {
+      this.$http
+        .get(`http://api.github.com/repos/${this.username}/${this.repo}/contents${this.path}`)
+        .then(response => {
+          this.files = response.data;
+        })
+    },
+    changePath(path) {
+      this.path = '/'+path;
+      this.fetchFiles();
+    },
+    goBack() {
+      this.path = this.path.split('/').slice(0, -1).join('/');
+      if (this.path === '') { this.path = '/'; }
+      this.fetchFiles();
+    }
+  },
+  created() {
+    if(this.username && this.repo) {
+      this.fetchFiles();
+    }
+  }
+}
+</script>
+
+<style>
+  .directory a{
+    cursor: pointer;
+    text-decoration: none;
+  }
+</style>
